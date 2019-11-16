81093ad2267d6bfa6e198c5ed7d35ec53af8a429
Matthew Caldwell <caldwellysr@gmail.com>
2019-11-16 16:13:55 -0600

    remove some comments

--- lib/spellbook_web/router.ex
+++ lib/spellbook_web/router.ex
@@ -10,4 +10,16 @@ defmodule SpellbookWeb.Router do
 
     resources "/spells", SpellController, except: [:new, :edit]
   end
+
+  # pipeline :browser do
+  #   plug :accepts, ["html"]
+  #   plug :fetch_session
+  #   plug :fetch_flash
+  #   plug :protect_from_forgery
+  #   plug :put_secure_browser_headers
+  # end
+
+  # scope "/", SpellbookWeb do
+  #   pipe_through :browser
+  # end
 end
